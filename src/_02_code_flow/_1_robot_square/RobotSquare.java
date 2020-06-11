6c9582a4e29e56eedede7685ec7cd3da7b63aeb8
root <root@daf203b1e1a0>
2020-06-11 23:59:38 +0000

    This is me

--- src/_02_code_flow/_1_robot_square/RobotSquare.java
+++ src/_02_code_flow/_1_robot_square/RobotSquare.java
@@ -12,24 +12,29 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotSquare {
 	
     public static void main(String[] args){
- 
-    	// This code makes a new Robot
-    	Robot rob = new Robot();
 
+    	// This code makes a new Robot
+    	Robot elliana = new Robot("mini");
+elliana.setX(325);
+elliana.setY(450);
+ elliana.sparkle();
         // Put the robot's pen down so it can draw
 		//        Use the     rob.penDown();   command.
-
+elliana.setRandomPenColor();
+elliana.setPenWidth(30);
+elliana.penDown();
         // SPEED. Make the robot move quickly.
 		//        Use the     rob.setSpeed();   command.
-
+elliana.setSpeed(100);
 
         // DRAW.  Draw a line
     	//		Use the     rob.move(); command
-
+for(int i = 0;i < 4; i++) {
+elliana.move(250);
 
 		// TURN.  Turn 90 degrees
     	//      Use the     rob.turn();  command
-
+elliana.turn(90); }
 
         // LOOP.  Now put the DRAW and TURN code into a for loop to repeat 4 times.
 		//        Did your Robot draw a square?
--- src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
+++ src/_02_code_flow/_2_robot_spiral/RobotSpiral.java
@@ -4,32 +4,34 @@ package _02_code_flow._2_robot_spiral;
  *    Level 0
  */
 
-
 import org.jointheleague.graphical.robot.Robot;
 
 public class RobotSpiral {
 
 	public static void main(String[] args) {
-		
-		// Create a new Robot
-		
-		// Set your robot's pen down 
-		
-		// SPEED. Set the robot to go at max speed (100)
-		
-        // LOOP. Use a for loop to repeat ALL the following lines of code 50 times. 
-		
-				// Change the robot pen color to random
-			
-				// DRAW.    Move the robot (5*i) pixels
-				//          "i" is the variable in the for loop	
-			
-				// TURN.    Turn the robot (360/7) degrees to the right
-			
-				// Change the robot pen width to (i)
 
-		
-		// Check the pattern against the picture in the recipe. If it matches, you are done!
-		
-	}
+		// Create a new Robot
+		Robot elliana = new Robot("batman");
+		// Set your robot's pen down
+elliana.sparkle();
+		elliana.penDown();
+
+		// SPEED. Set the robot to go at max speed (100)
+		elliana.setSpeed(75);
+		// LOOP. Use a for loop to repeat ALL the following lines of code 50 times.
+		for (int i = 0; i < 50; i++) {
+			// Change the robot pen color to random
+			elliana.setRandomPenColor();
+			// DRAW. Move the robot (10*i) pixels
+			// "i" is the variable in the for loop
+			elliana.move(3 * i);
+			// TURN. Turn the robot (360/3) degrees to the right
+			elliana.turn(360/ 3);
+			// Change the robot pen width to (i)
+			elliana.setPenWidth(i);
+
+			// Check the pattern against the picture in the recipe. If it matches, you are
+			// done!
+		}
+elliana.unSparkle();	}
 }
