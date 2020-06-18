80dfe0709858a084da87f0c00aa744dcec6975be
root <root@87d597c5ed75>
2020-06-18 23:36:22 +0000

    f

--- src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
+++ src/_02_code_flow/_3_pentagon_crazy/PentagonCrazy.java
@@ -12,29 +12,31 @@ public class PentagonCrazy {
 	private void drawPattern() {
 		
 		//  Create a new Robot
-
+Robot elliana = new Robot ();
+elliana.sparkle();
 		//  Put the robot's pen down
-
+elliana.penDown();
 		//  SPEED. Make the robot go at maximum speed (100)
-
+elliana.setSpeed(100);
 		//  COLOR. Set the pen to a color that you like for the shape
-
+elliana.setPenColor(Color.PINK);
 		//  NUMBER OF SIDES. Make an int variable for the number of sides the shape has.
 		//  				(Hint: its called PentagonCrazy)
-
+int pentagoncrazy = 5;
 		//  TURN ANGLE. Make another int variable for the angle you want the robot to turn. 
 		//  			Hint: Divide 360 by the number of sides the shape has.
-
+int angle = 360/pentagoncrazy;
 		//  LOOP. Use a for loop to repeat ALL the following lines of code 200 times. 
-		
+for (int i = 0; i < 200; i++) {		
 				//  DRAW.  Make the robot move "i" pixels
 				//         "i" is the variable in the for loop
-		
+elliana.move(i);	
 				//  TURN.  Turn the robot the amount in your angle variable + 1
+elliana.turn(angle + 50);
 
-
+}
 		// Hide the robot so you can see the pattern.
-		
+
 		// Check the pattern against the picture in the recipe. If it matches, you are done!
 	}
 
--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,29 +10,35 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
+Robot elliana = new Robot ();
+
+elliana.penDown();
 
 		// Set the robot speed to 100
-		
+elliana.setSpeed(100);
 		// Set the robot window size to 800 x 800
-
+elliana.setWindowSize(800, 800);
 		// Use a for loop to repeat all of the code below ONE time (we will change this later)
-
+for (int i = 0; i < 150; i++) {
+elliana.setRandomPenColor();
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+elliana.turn(360/8);
 			   // DRAW           Move the robot 64 pixels
-
+elliana.move(64);
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+elliana.turn(-40);			
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
+elliana.move(flameSize);
 				//               Turn the robot 170 degrees
-			
+elliana.turn(170);			
 				//               Move the robot the distance in the variable flameSize (again)
-			
+elliana.move(flameSize);
+elliana.setPenColor(Color.BLACK);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+elliana.turn(64);
 				// DRAW          Move the robot the distance in the variable baseSize
-		
+elliana.move(baseSize);
+}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
 		
