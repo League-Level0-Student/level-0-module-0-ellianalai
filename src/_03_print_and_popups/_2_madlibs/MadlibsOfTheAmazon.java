f5dfb5fa2a3ac830f16332e2474d9419078bfb24
root <root@5d0b8803d45f>
2020-06-28 19:56:28 +0000

    dkgbrbrgub

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -28,14 +28,14 @@ public class MadlibsOfTheAmazon {
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
 		String story = 
-		"Piranhas are more [**adjective**] during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"Piranhas are more " + adjective + " during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh" + liquid + "and will most\n"  +
+		"likely take a bite out of your " + bodyPart + "if you" + verb + "Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the" + place + "Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
-		// If you need to, move them around to make your story look better in the pop-up
+		// If you need to, move the//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////m around to make your story look better in the pop-up
 		JOptionPane.showMessageDialog(null,story);		
 		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
 
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,18 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String code = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
-		// 3. Otherwise, tell them to sign up for classes at The League.
-
+if (code.equals("yes")) {
+	JOptionPane.showMessageDialog(null,"You will rule the world");
+}
+else if (code .equals ("sort of")) {
+	JOptionPane.showMessageDialog(null,"Work more on coding and you will get there");
+}
+// 3. Otherwise, tell them to sign up for classes at The League.
+else {
+	JOptionPane.showMessageDialog(null,"Sign up for classes at The League");	
+}
 	}
 }
 
